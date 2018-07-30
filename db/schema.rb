49eccb62de21934e45a9bdac8996e82d5b9927fc
you saitou <you@saitouyuunoMacBook-Pro.local>
2018-07-30 15:36:55 +0900

    コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180730051028) do
-
+ActiveRecord::Schema.define(version: 20180730052749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
     t.string "password_digest"
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
